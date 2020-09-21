56be2bcd2164479a0d186cc16bed281f513eb277
Yue Su <34404957+yue-su@users.noreply.github.com>
2020-09-21 18:33:16 -0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,8 @@ module.exports = {
         path: `${__dirname}/src/images/`,
       },
     },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
   ],
   siteMetadata: {
     title: "Tingjun Han",
