e0f7e3052db4992f3f5aa7865409bddb9974073d
Yue Su <34404957+yue-su@users.noreply.github.com>
2020-09-22 01:22:04 -0300

    added particle bk

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,8 +10,6 @@ module.exports = {
         // },
       },
     },
-    // If you want to use styled components you should add the plugin here.
-    // 'gatsby-plugin-styled-components',
     "gatsby-plugin-react-helmet",
     {
       resolve: `gatsby-source-filesystem`,
