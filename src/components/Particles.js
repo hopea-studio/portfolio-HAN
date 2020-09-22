54a271c634b368a4665a37ace62d7819194ac9ec
Yue Su <34404957+yue-su@users.noreply.github.com>
2020-09-22 01:25:57 -0300

    Update Particles.js

--- src/components/Particles.js
+++ src/components/Particles.js
@@ -3,69 +3,66 @@ import ParticlesJS from "react-tsparticles"
 import { makeStyles } from "@material-ui/core/styles"
 
 const useStyles = makeStyles((theme) => ({
-
-    particles: 
-    {
-        position: "fixed",
-        zIndex: "-1",
-        left: '0'
-    }
+  particles: {
+    position: "fixed",
+    zIndex: "-1",
+    left: "0",
+  },
 }))
 
 const Particles = () => {
+  const classes = useStyles()
 
-    const classes = useStyles()
-
-    return (
-      <ParticlesJS
-        className={classes.particles}
-        height="100vh"
-        width="100vh"
-        id="tsparticles"
-        options={{
-          background: {
-            color: {
-              value: "#f8f9fa",
-            },
+  return (
+    <ParticlesJS
+      className={classes.particles}
+      height="100vh"
+      width="100vw"
+      id="tsparticles"
+      options={{
+        background: {
+          color: {
+            value: "#f8f9fa",
           },
-          fpsLimit: 60,
-          particles: {
-            color: {
-              value: "#311b92",
-            },
-            collisions: {
-              enable: true,
-            },
-            move: {
-              direction: "up",
-              enable: true,
-              outMode: "bounce",
-              random: true,
-              speed: 1,
-              straight: false,
-            },
-            number: {
-              density: {
-                enable: true,
-                value_area: 800,
-              },
-              value: 80,
-            },
-            opacity: {
-              value: 0.9,
-            },
-            shape: {
-              type: "triangle",
-            },
-            size: {
-              random: true,
-              value: 4,
-            },
+        },
+        fpsLimit: 60,
+        particles: {
+          color: {
+            value: "#311b92",
           },
-          detectRetina: true,
-        }}
-      />
-    )
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "up",
+            enable: true,
+            outMode: "bounce",
+            random: true,
+            speed: 1,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              value_area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.9,
+          },
+          shape: {
+            type: "triangle",
+          },
+          size: {
+            random: true,
+            value: 4,
+          },
+        },
+        detectRetina: true,
+      }}
+    />
+  )
 }
 
 export default Particles
